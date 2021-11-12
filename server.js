659dd6a0690ce1288a71dab16a65a81c5f32afb1
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-13 06:59:45 +0800

    variables function, created app express

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-// 
+// variables function
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db")
+// express app set
+const app = express();
+const PORT = 3000;
+// data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
