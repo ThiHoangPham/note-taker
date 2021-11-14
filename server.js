ee8992a0bf329f9298b6d58539cc120e8d9507fa
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-14 11:03:01 +0800

    fixed err: Cannot GET /notes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // variables function
 const express = require("express");
 const fs = require("fs");
-const { get } = require("http");
 const path = require("path");
 const database = require("./db/db")
 
 // express app set
-const app = express();
-const PORT = 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 // data parsing
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,37 +17,40 @@ app.use(express.json());
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-})
 
-//get and post function
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 app.route("/api/notes")
+    // get function
     .get(function (req, res) {
         res.json(database);
     })
+
+    // post function
     .post(function (req, res) {
         let jsonFilePath = path.join(__dirname, "/db/db.json");
         let newNote = req.body;
         let highestId = 99;
-        for (let i = 0; 1 < database.length; i++) {
+        for (let i = 0; i < database.length; i++) {
             let individualNote = database[i];
             if (individualNote.id > highestId) {
                 highestId = individualNote.id;
             }
         }
         newNote.id = highestId + 1;
-        database.push(newNote);
+        database.push(newNote)
         fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
             if (err) {
                 return console.log(err);
             }
-            console.log("Your notes were saved");
+            console.log("Your note was saved!");
         });
+        res.json(newNote);
     });
 
 // delete function
-app.delete("api/note/:id", function (req, res) {
+app.delete("/api/notes/:id", function (req, res) {
     let jsonFilePath = path.join(__dirname, "/db/db.json");
     for (let i = 0; i < database.length; i++) {
         if (database[i].id == req.params.id) {
@@ -59,12 +62,13 @@ app.delete("api/note/:id", function (req, res) {
         if (err) {
             return console.log(err);
         } else {
-            console.log("Your Notes Were Deleted!")
+            console.log("Your note was deleted!");
         }
     });
+    res.json(database);
 });
 
 // final setup for server
-app.listen(PORT, function () {
-    console.log("App listening on PORT" + PORT);
-});
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT" + PORT);
+// });
